cf4950d19f6c87156df8bbf4be3bd91e1041f565
xjshdisoaj14 <139882166+xjshdisoaj14@users.noreply.github.com>
2024-08-01 23:50:56 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.123.42/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.123.42/g' package/base-files/files/bin/config_generate
 
 # ==========openclash===========
  cd package
