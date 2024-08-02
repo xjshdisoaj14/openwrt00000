4b7d6b0aa5b8ac0c97c540f2b825f260e1eb8347
xjshdisoaj14 <139882166+xjshdisoaj14@users.noreply.github.com>
2024-08-02 12:31:23 +0800

    Update arm64v8-part2.sh

--- arm64v8-part2.sh
+++ arm64v8-part2.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.123.42/g' package/base-files/files/bin/config_generate
 
 git clone https://github.com/ophub/luci-app-amlogic.git package/luci-app-amlogic
 
