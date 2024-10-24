9535514544fd77d490fcd72cd4b56d7f3acc4ce3
simpxing <58797326+simpxing@users.noreply.github.com>
2024-10-24 19:27:01 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -3,8 +3,6 @@
 # Modify default IP
  sed -i 's/192.168.1.1/192.168.123.32/g' package/base-files/files/bin/config_generate
 
-# ==========openclash===========
-# cd package
-#  git clone --depth=1 --filter=blob:none --sparse https://github.com/vernesong/OpenClash.git
-# cd OpenClash
-#  git sparse-checkout set luci-app-openclash
+# Modify default theme
+#sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+
