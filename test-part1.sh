eaabee65333d17a650f77bb5e4456d48a2f72122
simpxing <58797326+simpxing@users.noreply.github.com>
2024-10-27 19:10:44 +0800

    Update test-part1.sh

--- test-part1.sh
+++ test-part1.sh
@@ -7,6 +7,6 @@
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 #sed -i '$a src-git helloworld https://github.com/fw876/helloworld.git' ./feeds.conf.default
-echo 'src-git small https://github.com/sinpxing/small' >>feeds.conf.default
-# echo 'src-git opentopd  https://github.com/sirpdboy/sirpdboy-package' >>feeds.conf.default
-sed -i '$a src-git openwrtpackages https://github.com/sinpxing/openwrt-packages.git' ./feeds.conf.default
+echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
+#echo 'src-git opentopd  https://github.com/sirpdboy/sirpdboy-package' >>feeds.conf.default
+sed -i '$a src-git openwrtpackages https://github.com/kenzok8/openwrt-packages.git' ./feeds.conf.default
