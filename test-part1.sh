bfb03ba506a6ce1959a46e8f00f2f9f0cee59d63
sinpxing <70903525+sinpxing@users.noreply.github.com>
2024-11-30 10:57:23 +0800

    Update test-part1.sh

--- test-part1.sh
+++ test-part1.sh
@@ -10,3 +10,5 @@
 echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
 echo 'src-git opentopd  https://github.com/sirpdboy/sirpdboy-package' >>feeds.conf.default
 sed -i '$a src-git openwrtpackages https://github.com/kenzok8/openwrt-packages.git' ./feeds.conf.default
+# homeproxy
+git clone --depth=1 https://github.com/immortalwrt/homeproxy.git package/homeproxy
