112cccf19271f556312f1e6493cebe91b17f3e01
sinpxing <70903525+sinpxing@users.noreply.github.com>
2024-12-02 19:18:48 +0800

    Update test-part1.sh

--- test-part1.sh
+++ test-part1.sh
@@ -10,4 +10,3 @@
 echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
 echo 'src-git opentopd  https://github.com/sirpdboy/sirpdboy-package' >>feeds.conf.default
 sed -i '$a src-git openwrtpackages https://github.com/kenzok8/openwrt-packages.git' ./feeds.conf.default
-
