d208c1f400a25670bf4ed3bf649c9501186782db
sinpxing <70903525+sinpxing@users.noreply.github.com>
2025-01-13 22:28:00 +0800

    Update test-part1.old.sh

--- test-part1.old.sh
+++ test-part1.old.sh
@@ -10,5 +10,10 @@
 echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
 echo 'src-git opentopd  https://github.com/sirpdboy/sirpdboy-package' >>feeds.conf.default
 sed -i '$a src-git openwrtpackages https://github.com/kenzok8/openwrt-packages.git' ./feeds.conf.default
+
+
+sed -i '$a src-git helloworld https://github.com/fw876/helloworld.git' ./feeds.conf.default
+echo "src-git mihomo https://github.com/morytyann/OpenWrt-mihomo.git;main" >> "feeds.conf.default"
+
 # homeproxy
 git clone --depth=1 https://github.com/immortalwrt/homeproxy.git package/homeproxy
